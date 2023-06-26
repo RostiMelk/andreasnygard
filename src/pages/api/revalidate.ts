1db9b764cdb899f74c51a2dfb9936c76994986cc
RostiMelk <hello@rosti.no>
2023-06-26 19:00:45 +0200

    revalidation of refs

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -4,6 +4,11 @@ import { parseBody } from "next-sanity/webhook";
 // Export the config from next-sanity to enable validating the request body signature properly
 export { config } from "next-sanity/webhook";
 
+const revalidatationMap: { [key: string]: string[] } = {
+  work: ["/", "/work/[slug]"],
+  blog: ["/blog", "/blog/[slug]"],
+};
+
 export default async function revalidate(
   req: NextApiRequest,
   res: NextApiResponse
@@ -29,10 +34,22 @@ export default async function revalidate(
       return;
     }
 
-    const staleRoute = `/${body._type}/${slug.current}`;
-    await res.revalidate(staleRoute);
-    const message = `Updated route: ${staleRoute}`;
-    return res.status(200).json({ message });
+    const type = body._type;
+
+    if (!revalidatationMap[type]) {
+      const message = `No revalidation map for type: ${type}`;
+      console.warn(message);
+      res.status(400).json({ message });
+      return;
+    }
+
+    for (const route of revalidatationMap[type] as string[]) {
+      const staleRoute = route.replace("[slug]", slug.current);
+      console.log(`Revalidating route: ${staleRoute}`);
+      await res.revalidate(staleRoute);
+    }
+
+    return res.status(200).json({ message: "OK" });
   } catch (err) {
     console.error(err);
     return res.status(500).json({ message: "Internal server error" });
