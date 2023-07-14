38a0082ff79ea64644278f0899bc38f31aefb49d
RostiMelk <hello@rosti.no>
2023-07-14 20:53:27 +0200

    transitions

--- src/components/ImageRow.tsx
+++ src/components/ImageRow.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Image from "next/legacy/image";
 import type { SanityImageAssetDocument } from "@sanity/client";
 import clsx from "@/lib/clsx";
@@ -13,16 +13,48 @@ interface Props {
   }[];
 }
 
+/**
+ * Image width is based on the number of images in the row.
+ */
+const imageWidth: Record<number, number> = {
+  1: 1800,
+  2: 900,
+  3: 600,
+};
+
 export const ImageRow = ({ className, imageRow }: Props) => {
-  const maxWidth: Record<number, number> = {
-    1: 1800,
-    2: 900,
-    3: 600,
-  };
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          ref.current?.classList.add("opacity-100");
+        }
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.25,
+      }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
   return (
     <section
-      className={clsx("mb-8 flex flex-col gap-8 md:flex-row", className)}
+      ref={ref}
+      className={clsx(
+        "mb-8 flex flex-col gap-8 opacity-0 transition-all  duration-500 md:flex-row",
+        className
+      )}
     >
       {imageRow?.map(({ asset, _key }) => (
         <div key={_key} className="flex-1">
@@ -34,7 +66,7 @@ export const ImageRow = ({ className, imageRow }: Props) => {
             placeholder="blur"
             quality={100}
             src={urlFor(asset)
-              .width(maxWidth[imageRow.length] ?? 1800)
+              .width(imageWidth[imageRow.length] ?? 1800)
               .quality(85)
               .url()}
             width={asset?.metadata?.dimensions?.width ?? 0}
